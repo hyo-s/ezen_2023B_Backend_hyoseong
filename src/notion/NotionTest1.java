801bebca66ae6eda0240d4c8c982cae0a4d21684
lococo0425 <lococo0425@naver.com>
2024-03-04 19:02:29 +0900

    202240304

---
+++ src/notion/NotionTest1.java
@@ -0,0 +1,13 @@
+package notion;
+
+public class NotionTest1 {
+    public static void main(String[] args) {
+
+        int count;
+            // 변수 선언은 저장되는 값의 타입과 이름만 결정한 것이지 아직 메모리에 할당 된 것이 아니다.
+        count = 90;
+            // 변수에 최초로 값이 대입 될 때 메모리에 할당되고, 해당 메모리에 값이 저장된다.
+            // 변수에 최초로 값을 대입하는 행위를 변수  초기화라고 한다. 이때 값을 초기값이라고 한다.
+
+    }
+}
